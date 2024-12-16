653664de804e37a4f259f1faebfbc8c9b5aa626d
Rohan Bansal <rohan@agritheory.dev>
2024-12-16 16:30:13 +0530

    fix: scan doctype files

--- nuxt/src/module.ts
+++ nuxt/src/module.ts
@@ -1,4 +1,6 @@
 import { defineNuxtModule, addPlugin, createResolver, extendPages } from '@nuxt/kit'
+import { existsSync, readdirSync } from 'node:fs'
+import { extname } from 'node:path'
 
 const { resolve } = createResolver(import.meta.url)
 
@@ -9,9 +11,25 @@ export default defineNuxtModule({
 	},
 
 	setup(_options, _nuxt) {
+		const rootDir = _nuxt.options.srcDir
+		const doctypesDir = resolve(rootDir, 'doctypes')
+		if (existsSync(doctypesDir)) {
+			const schemas = readdirSync(doctypesDir).filter(file => extname(file) === '.json')
+
+			for (const schema of schemas) {
+				const schemaName = schema.replace('.json', '')
+				const schemaPath = resolve(doctypesDir, schema)
+				extendPages(pages => {
+					pages.unshift({
+						name: `stonecrop-${schemaName}`,
+						path: `/${schemaName}`,
+						file: 'AForm',
+					})
+				})
+			}
+		}
+
 		// Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
 		addPlugin(resolve('./runtime/plugin'))
-
-		extendPages(pages => {})
 	},
 })
--- nuxt/src/runtime/plugin.ts
+++ nuxt/src/runtime/plugin.ts
@@ -1,3 +1,4 @@
+import { AForm } from '@stonecrop/aform'
 import { Stonecrop } from '@stonecrop/stonecrop'
 import { useRouter } from 'nuxt/app'
 
@@ -5,5 +6,6 @@ import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin(_nuxtApp => {
 	const app = _nuxtApp.vueApp
+	app.component('AForm', AForm)
 	app.use(Stonecrop)
 })
