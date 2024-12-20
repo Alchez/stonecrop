d8ebd163fb1ca29b6a1640ddc7cea65de32de0ec
Rohan Bansal <rohan@agritheory.dev>
2024-12-20 15:46:39 +0530

    fix: implement server-side data fetching

--- nuxt/playground/nuxt.config.ts
+++ nuxt/playground/nuxt.config.ts
@@ -1,7 +1,9 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
+import NuxtStonecrop from '../src/module'
+
 export default defineNuxtConfig({
-	modules: ['../src/module'],
+	modules: [NuxtStonecrop],
 	stonecrop: {},
 	devtools: { enabled: true },
 	compatibilityDate: '2024-12-13',
--- nuxt/src/runtime/pages/StonecropPage.vue
+++ nuxt/src/runtime/pages/StonecropPage.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>Stonecrop Page</div>
-	<AForm v-model="schema" />
+	<AForm v-model="schema" :data="schemaData" :key="componentKey" />
 </template>
 
 <script setup lang="ts">
@@ -10,9 +10,17 @@ import { onMounted, ref } from 'vue'
 
 const route = useRoute()
 const schema = ref<SchemaTypes[]>([])
+const schemaData = ref<any[]>([])
+const componentKey = ref(0)
 
-onMounted(() => {
+onMounted(async () => {
 	// wait for the route-based schema to be resolved
 	schema.value = route.meta.schema as SchemaTypes[]
+
+	const res = await $fetch(`/api${route.path}`)
+	schemaData.value = res as any[]
+
+	// re-render form when data is available
+	componentKey.value++
 })
 </script>
